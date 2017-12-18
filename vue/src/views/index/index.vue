66d36c8aaba6e2e5ef5521b9d90a1596547db754
KidneyFlower <753475777@qq.com>
2017-12-18 21:54:59 +0800

    :sparkles: 初始化 Vue 项目

---
+++ vue/src/views/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="v-home">
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped lang="scss">
+  @import './style.scss';
+</style>
