e94f18deabbc9dae446f4a860bf07e08bc9e91d5
KidneyFlower <753475777@qq.com>
2017-12-20 17:59:05 +0800

    :sparkles: 对应用主体和登录注册两部分的路由进行了分层

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-const Home = () => import('@/views/index')
+// 登录注册部分
+const LoginLayout = () => import('@/views/login/layout')
+const Login = () => import('@/views/login/index')
+
+// 应用主体部分
+const AppLayout = () => import('@/views/app/layout')
+const Index = () => import('@/views/app/index')
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      component: Home
+      path: '/app',
+      component: AppLayout,
+      children: [
+        {
+          path: '/',
+          component: Index
+        }
+      ]
+    },
+    {
+      path: '/login',
+      component: LoginLayout,
+      children: [
+        {
+          path: '/',
+          component: Login
+        }
+      ]
+    },
+    {
+      path: '/*',
+      redirect: '/app'
     }
   ]
 })
